7689b45cd5a4484b14cfc300a0c75d580df2b6c9
saraulayya <126043516+saraulayya@users.noreply.github.com>
2023-11-06 23:19:24 +0700

    Add files via upload

---
+++ decafe/home.php
@@ -0,0 +1,13 @@
+<div class="col-lg-9 mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        Home
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ini Adalah Bagian Home</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est impedit id consequatur possimus. Assumenda laborum rerum autem excepturi! Error sint consequuntur, in molestias provident inventore aspernatur ab dolorum eveniet aliquid.
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
