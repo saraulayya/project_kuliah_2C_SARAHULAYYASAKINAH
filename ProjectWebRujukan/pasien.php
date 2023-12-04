af81fc47a4fa1b253d97c99491560690270cd9c4
saraulayya <126043516+saraulayya@users.noreply.github.com>
2023-12-04 23:32:34 +0700

    Add files via upload

---
+++ ProjectWebRujukan/pasien.php
@@ -0,0 +1,16 @@
+<div class="col-lg-9 mt-2">
+                <div class="card text-center">
+                    <div class="card-header">
+                        Pasien
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">WELCOME TO Pasien</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sed facilis cumque fugiat, aut, magnam fuga quia culpa repudiandae distinctio saepe. Esse laboriosam aliquid voluptas inventore porro eaque et deleniti? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt tenetur delectus vitae quod cumque porro eveniet totam, impedit ex necessitatibus perferendis harum cum quae error veniam praesentium architecto nemo consequatur!
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-body-secondary">
+                        2 days ago
+                    </div>
+                </div>
+            </div>
